174705594197a1832b5d4e044306c7ad449bef9b
sboosaraju <sboosaraju@emtrain.com>
2022-11-10 23:12:25 +0530

    green'

---
+++ angular-frontend-green/src/app/create-user/create-user.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../user';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent implements OnInit {
+  user: User = new User();
+  constructor(private userService:UserService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+  saveUser(){
+    this.userService.createUser(this.user).subscribe(data=>{
+      console.log(data);
+      this.goToUsers();
+    },
+    error => console.log(error)
+    );
+  }
+
+  goToUsers(){
+     this.router.navigate(['/users']);
+  }
+  onSubmit(){
+    console.log(this.user);
+    this.saveUser();
+  }
+
+}
--- angular-frontend-red/src/app/user.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-    id!: number;
-    firstName!: string;
-    lastName!: string;
-    email!: string;
-
-
-}
